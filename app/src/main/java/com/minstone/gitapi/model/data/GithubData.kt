b697d674f1822f0c35953dc7182942494c88dbfc
GSM-Minseok <ms0818000@gmail.com>
2023-03-24 10:52:06 +0900

    fix : SerializedName 수정

--- app/src/main/java/com/minstone/gitapi/model/data/GithubData.kt
+++ app/src/main/java/com/minstone/gitapi/model/data/GithubData.kt
@@ -5,10 +5,8 @@ import com.google.gson.annotations.SerializedName
 data class GithubData (
     @SerializedName("login") var userName : String?,
     @SerializedName("id")var userId : String?,
-    @SerializedName("avartar_url")var userProfile:String?,
-    @SerializedName("html_url")var htmlUrl:String?,
+    @SerializedName("avatar_url")var userProfile:String?,
     @SerializedName("bio")var bio:String?,
-    @SerializedName("follwers")var followers:Int?,
+    @SerializedName("followers")var followers:Int?,
     @SerializedName("following")var following:Int?
-
 )
